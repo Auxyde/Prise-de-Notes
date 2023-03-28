de750589e066c3c65c0b551b8067e379f0fc962e
PERRIER <m1x6me@gmail.com>
2023-03-28 16:26:45 +0200

    + Ajout "Se connecter" & "S'inscrire"

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { MdStickyNote2 as Icon } from "react-icons/md";
+
 function Header() {
-  return (
-    <header>
-      <h1>
-        <Icon viewBox="0 0 20 20" />
-        &nbsp;Notepad
-      </h1>
-    </header>
-  );
+    return (
+        <header>
+            <h1>
+                <Icon viewBox="0 0 20 20" />
+                &nbsp;Notepad
+            </h1>
+            <div className="header-right">
+                <a href="#">Se connecter</a>
+                <a href="#">S'inscrire</a>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
