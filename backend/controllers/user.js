02716ef29288f5f79baaa6b1c6e5a2f88112a76c
ratel4 <alexandre.ratel@etu.unilim.fr>
2023-03-29 15:49:59 +0200

    Add users query and user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -76,7 +76,70 @@ const auth_user = (req, res) => {
       });
   });
 };
+
+// Liste des utilisateurs
+// (_id,name, firstname,mail)
+const get_users = (req, res) =>{
+    connectDB((cnx) => {
+        User.find()
+        .then((result) => {
+            closeDB();
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            closeDB();
+            console.log("Crashed")
+            res.status(204).json(err);
+        });
+    });
+};
+
+// Recherche d’un utilisateur à
+// partir de son : _id, name,
+// firstname ou mail
+
+// Dans notre cas, on ira du plus précis au moins précis, donc dans un premier temps _id et mail, puis name et firstname
+// On pourra éventuellement combiner firstname et name pour une recherche plus précise
+const get_user = (req, res) => {
+    const searchParams = req.params.val;
+    let columns = ["mail", "name", "firstname"];
+    if(searchParams.length == 24) columns.push("_id")
+    let query = {$or : []};
+    columns.forEach((col) => {
+        let obj = {};
+        obj[col] =  searchParams;
+        query.$or.push(obj);
+    });
+
+
+    console.log( query);
+    connectDB((cnx) => {
+        User.find(query)
+            .then((result) => {
+                console.log(result);
+                res.status(200).json(result);
+                closeDB();
+            })
+            .catch((err) => {
+                console.log(err);
+                closeDB();
+                res.status(204).json(err);
+
+            });
+        })
+};
+
+//Liste des notes d’un auteur
+// (_id)
+const get_user_notes = (req, res) => {
+    //TODO
+};
+
 module.exports = {
-  insert_user,
-  auth_user,
+    insert_user,
+    auth_user,
+    get_users,
+    get_user,
+    get_user_notes,
 };
