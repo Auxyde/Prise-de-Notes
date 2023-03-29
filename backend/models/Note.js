ade70da3f7c9b8808f485a2de7a278245eb7c5f7
ratel4 <alexandre.ratel@etu.unilim.fr>
2023-03-29 16:22:25 +0200

    Add note model

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -3,5 +3,14 @@ const mongoose = require("mongoose");
 const NoteSchema = new mongoose.Schema({
     title: String,
     content: String,
-    author: String,
+    author: { type: String, required: [true, "Auteur obligatoire"] },
 });
+
+NoteSchema.pre("save", function (next) {
+    const note = this;
+});
+
+NoteSchema.pre("findOneAndUpdate", function (next) {
+    const note = this;
+    const update = note.getUpdate().$set;
+}
