c79afcd8a939eb0c32f2886d7a3bb605e114c582
ratel4 <alexandre.ratel@etu.unilim.fr>
2023-03-29 16:54:57 +0200

    Ajout du get_notes

--- backend/controllers/note.js
+++ backend/controllers/note.js
@@ -2,8 +2,23 @@ const { connectDB, closeDB } = require("../db/conn");
 const Note = require("../models/Note");
 const { jwtSign } = require("../secure");
 
+//Liste de toutes les notes
+// triées par title
 const get_notes = function (req, res) {
-    //TODO
+    connectDB((cnx) => {
+        Note.find()
+            .sort({ title: 1 })
+            .then((data) => {
+                console.log(data);
+                closeDB();
+                res.status(200).json(data);
+            })
+            .catch((err) => {
+                console.log(err);
+                closeDB();
+                res.status(204).json(err);
+            });
+    });
 }
 
 const get_note = function (req, res) {
