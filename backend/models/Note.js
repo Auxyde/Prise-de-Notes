2a0f218b93d3ade1cb81cb62e2e40784aa41144a
ratel4 <alexandre.ratel@etu.unilim.fr>
2023-03-29 16:43:52 +0200

    Fixed Note model

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -8,9 +8,15 @@ const NoteSchema = new mongoose.Schema({
 
 NoteSchema.pre("save", function (next) {
     const note = this;
+    next();
 });
 
 NoteSchema.pre("findOneAndUpdate", function (next) {
     const note = this;
     const update = note.getUpdate().$set;
-}
+    note.setUpdate(update);
+    next();
+});
+
+/* Liaison du model à la collection */
+module.exports = mongoose.model("notes", NoteSchema);
