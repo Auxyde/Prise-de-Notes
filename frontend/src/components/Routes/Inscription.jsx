448799ed9d527aa66d9891b3194eba3c1799e20b
PERRIER <m1x6me@gmail.com>
2023-04-02 22:53:16 +0200

    + Inscription.jsx & MyNotes.jsx

--- frontend/src/components/Routes/Inscription.jsx
+++ frontend/src/components/Routes/Inscription.jsx
@@ -1,34 +1,94 @@
+import React, { useState } from "react";
+import axios from "axios";
 import Header from "../Header";
 import Footer from "../Footer";
 
 function Inscription() {
+    const [nom, setNom] = useState("");
+    const [prenom, setPrenom] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newUser = { nom, prenom, email, password };
+        axios
+            .post("http://localhost:3001/users", newUser)
+            .then(() => {
+                console.log("Utilisateur ajouté avec succès!");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     return (
         <div>
             <Header />
             <div className="signup-block">
                 <h2>Inscription</h2>
-                <form className="forminscription">
+                <form className="forminscription" onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="nom">Nom :</label>
-                        <input type="text" id="nom" name="nom" placeholder="Nom" required />
+                        <input
+                            type="text"
+                            id="nom"
+                            name="nom"
+                            placeholder="Nom"
+                            required
+                            className="form-control"
+                            value={nom}
+                            onChange={(e) => setNom(e.target.value)}
+                        />
                     </div>
-                    <div className="form-group" id="prenom-group">
+                    <div className="form-group">
                         <label htmlFor="prenom">Prénom :</label>
-                        <input type="text" id="prenom" name="prenom" placeholder="Prénom" required />
+                        <input
+                            type="text"
+                            id="prenom"
+                            name="prenom"
+                            placeholder="Prénom"
+                            required
+                            className="form-control"
+                            value={prenom}
+                            onChange={(e) => setPrenom(e.target.value)}
+                        />
                     </div>
-                    <div className="form-group" id="email-group">
+                    <div className="form-group">
                         <label htmlFor="email">Mail :</label>
-                        <input type="email" id="email" name="email" placeholder="Mail" required />
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Mail"
+                            required
+                            className="form-control"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
-                    <div className="form-group" id="password-group">
+                    <div className="form-group">
                         <label htmlFor="password">Mot de passe :</label>
-                        <input type="password" id="password" name="password" placeholder="Mot de passe" required />
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Mot de passe"
+                            required
+                            className="form-control"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
-                    <button type="submit">S'inscrire</button>
-                    <div className="login-link">
-                        <a href="/connexion" style={{ color: "#f5ba13" }}>J'ai déjà un compte</a>
-                    </div>
+                    <button type="submit" className="btn btn-primary">
+                        S'inscrire
+                    </button>
                 </form>
+                <div className="login-link">
+                    <a href="/connexion" style={{ color: "#f5ba13" }}>
+                        J'ai déjà un compte
+                    </a>
+                </div>
             </div>
             <Footer />
         </div>
--- frontend/src/components/Routes/MyNotes.jsx
+++ frontend/src/components/Routes/MyNotes.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function MyNotes() {
+    const [notes, setNotes] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/notes")
+            .then((response) => {
+                setNotes(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const filteredNotes = notes.filter(
+        (note) => note.userId === 1 // Remplacer "1" par l'id de l'utilisateur en cours
+    );
+
     return (
         <div>
             <h1>My Notes</h1>
+            <ul>
+                {filteredNotes.map((note) => (
+                    <li key={note.id}>
+                        <h2>{note.title}</h2>
+                        <p>{note.content}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
+
 export default MyNotes;
